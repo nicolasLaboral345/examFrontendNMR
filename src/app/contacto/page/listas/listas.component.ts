dff85b3f137e66d997aff61779b7dd5bb03cbbde
nicolasLaboral345 <laboralnicolasmoreno@gmail.com>
2022-07-08 15:18:47 -0500

    contactos lista completo

--- src/app/contacto/page/listas/listas.component.ts
+++ src/app/contacto/page/listas/listas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactosServService } from 'src/app/contactos-serv.service';
 
 @Component({
   selector: 'app-listas',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListasComponent implements OnInit {
 
-  constructor() { }
+  constructor(public datos: ContactosServService) {
+    this.listContactos = this.datos.listContactos
+   }
 
   ngOnInit(): void {
   }
+
+  
+  filtroCorreo = "";
+  filtroTelefono = "";
+  filtroFecha: any;
+
+  
+  public listContactos = [
+    {
+      nombre: "NaN",
+      telefono: 0,
+      fechaNacimiento: "NaN",
+      direccion: "NaN",
+      correo: "NaN"
+    }
+  ]
+
+  
+
+
+  calcularEdad(fecha: any) {
+    var hoy = new Date();
+    var cumpleanos = new Date(fecha);
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    var m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+
+    return edad;
+  }
+  
+
 
 }
---
+++ src/app/contactos-serv.service.ts
@@ -0,0 +1,140 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactosServService {
+
+  constructor() { }
+
+  public listContactos = [
+    {
+      nombre: "paula rodrigues",
+      telefono: 321568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example1@gmail.com"
+    },
+    {
+      nombre: "camilo",
+      telefono: 322568234,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example3@gmail.com"
+    },
+    {
+      nombre: "daniela bustamente quesada",
+      telefono: 300568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example2@gmail.com"
+    },
+    {
+      nombre: "paula rodrigues",
+      telefono: 321568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example1@gmail.com"
+    },
+    {
+      nombre: "camilo",
+      telefono: 322568234,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example3@gmail.com"
+    },
+    {
+      nombre: "daniela bustamente quesada",
+      telefono: 300568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example2@gmail.com"
+    },
+    {
+      nombre: "paula rodrigues",
+      telefono: 321568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example1@gmail.com"
+    },
+    {
+      nombre: "camilo",
+      telefono: 322568234,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example3@gmail.com"
+    },
+    {
+      nombre: "daniela bustamente quesada",
+      telefono: 300568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example2@gmail.com"
+    },
+    {
+      nombre: "paula rodrigues",
+      telefono: 321568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example1@gmail.com"
+    },
+    {
+      nombre: "camilo",
+      telefono: 322568234,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example3@gmail.com"
+    },
+    {
+      nombre: "daniela bustamente quesada",
+      telefono: 300568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example2@gmail.com"
+    },
+    {
+      nombre: "paula rodrigues",
+      telefono: 321568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example1@gmail.com"
+    },
+    {
+      nombre: "camilo",
+      telefono: 322568234,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example3@gmail.com"
+    },
+    {
+      nombre: "daniela bustamente quesada",
+      telefono: 300568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example2@gmail.com"
+    },
+    {
+      nombre: "paula rodrigues",
+      telefono: 321568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example1@gmail.com"
+    },
+    {
+      nombre: "camilo",
+      telefono: 322568234,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example3@gmail.com"
+    },
+    {
+      nombre: "daniela bustamente quesada",
+      telefono: 300568943,
+      fechaNacimiento: "1998-07-19",
+      direccion: "cra 12a # 167 - 91",
+      correo: "example2@gmail.com"
+    },
+  ]
+
+  public filtroNombre = ""
+}
